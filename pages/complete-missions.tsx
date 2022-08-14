bd017831d7ea842200f255d17570c8249e95226d
ekrte <ekrte@github.com>
2022-08-15 02:38:15 +0900

    Add: 참여한 미션 없는 경우 페이지 추가

--- pages/complete-missions.tsx
+++ pages/complete-missions.tsx
@@ -1,10 +1,39 @@
 import { BackHeader, StickyHeader } from "../layouts/header";
 import { PageContainer } from "../layouts";
-import { useRouter } from "next/router";
+import Image from "next/image";
 import isLoggedIn from "../hooks/isLoggedIn";
 import { useEffect, useState } from "react";
 import CompleteMissionSection, { MissionType } from "../components/complete-missions/CompleteMissionSection";
 import getCompleteMissions from "../apis/getCompleteMissions";
+import styled from "styled-components";
+
+const EmptyMissionSection = styled.div`
+	height: 100%;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	flex: 1;
+	
+	.mission-empty-container {
+		display: flex;
+		flex-direction: column;
+		row-gap: 40px;
+		align-items: center;
+	}
+
+	.mission-empty-description {
+		color: ${(props) => props.color ?? props.theme.colors.gray60};
+	}
+`;
+ 
+const EmptyMissionElement = () => (
+	<EmptyMissionSection>
+		<div className="mission-empty-container">
+			<Image src={"/image/emptyNoti.svg"} width={"228px"} height={"187px"} alt="empty Noti Image" />
+			<span className="mission-empty-description">참여한 미션이 없어요!</span>
+		</div>
+	</EmptyMissionSection>
+);
 
 function CompleteMissions() {
 	isLoggedIn();
@@ -24,9 +53,13 @@ function CompleteMissions() {
 					onBack={() => history.back()}
 				/>
 			</StickyHeader>
-			<CompleteMissionSection 
-				missions={missions}
-			/>
+			
+			{missions.length > 0 
+				? <CompleteMissionSection
+					missions={missions}
+				/>
+				: <EmptyMissionElement/>
+			}
 		</PageContainer>
 	);
 }
