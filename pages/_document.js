d42490e54a6f7e04ad55bbc51e8d835ec70b589f
ekrte <ekrte@github.com>
2022-08-15 18:31:19 +0900

    Add: favicon 추가

--- pages/_document.js
+++ pages/_document.js
@@ -13,6 +13,7 @@ class MyDocument extends Document {
 						href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap"
 						rel="stylesheet"
 					/>
+					<link rel="shortcut icon" href="/icon/favicon/favicon.png" />
 				</Head>
 				<body className="mobile-container">
 					<Main />
